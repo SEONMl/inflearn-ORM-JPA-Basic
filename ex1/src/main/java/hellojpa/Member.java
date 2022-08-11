edf522d0994363652c74ac8bcc0fc59de2e28508
SEONMl <sm000117@naver.com>
2022-08-11 23:21:43 +0900

    Section6: 22강 일대다 매핑

--- ex1/src/main/java/hellojpa/Member.java
+++ ex1/src/main/java/hellojpa/Member.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Getter
@@ -13,35 +14,14 @@ import java.util.Date;
 @NoArgsConstructor
 public class Member {
     @Id
+    @GeneratedValue
+    @Column(name="member_id")
     private Long id;
     private String name;
 
-    private Integer age;
-
-//    @Column(name="Team_id")
-//    private Long teamId;
-
-    @ManyToOne // 멤버 입장에서 many
-    @JoinColumn(name = "team_id") // 관계할 때 조인할 칼럼
+    // 일대다 양방향ㅎ
+    @ManyToOne
+    @JoinColumn(name="team_id",insertable = false, updatable = false)
     private Team team;
-
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob // varchar를 넘어서는 큰 필드
-    private String description;
-
     // 로직이 들어가면 getter, setter 사용 지양
-    public void changeTeam(Team team) {
-        this.team = team;
-        // 순수 객체 상태를 고려해서 항상 양쪽에 값을 설정하자
-        team.getMembers().add(this);
-    }
-
 }
--- ex1/src/main/java/hellojpa/Team.java
+++ ex1/src/main/java/hellojpa/Team.java
@@ -15,17 +15,23 @@ import java.util.List;
 public class Team {
     @Id
     @GeneratedValue
+    @Column(name="team_id")
     private Long id;
     private String name;
+
+    //
+    @OneToMany
+    @JoinColumn(name="team_id")
+    private List<Member> members = new ArrayList<>();
 
     // Member에서 넣을건지 Team에서 넣을건지 기준을 정하기
     // 양쪽에 연관관계 있으면 문제 발생 확률 UP
     // 이런 식으로도 가능
-    public void addMember(Member member){
-        member.setTeam(this);
-        members.add(member);
-    }
+//    public void addMember(Member member){
+//        member.setTeam(this);
+//        members.add(member);
+//    }
 
-    @OneToMany(mappedBy = "team")
-    private List<Member> members = new ArrayList<>(); // ArrayList로 초기화 해 주는 것이 관례
+//    @OneToMany(mappedBy = "team")
+//    private List<Member> members = new ArrayList<>(); // ArrayList로 초기화 해 주는 것이 관례
 }
