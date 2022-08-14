6f2b023efea2b091710b6858c6de2e0e50e29170
SEONMl <sm000117@naver.com>
2022-08-15 00:32:09 +0900

    Section9: 37강 값 타입 컬렉션

--- ex1/src/main/java/hellojpa/Member.java
+++ ex1/src/main/java/hellojpa/Member.java
@@ -6,6 +6,10 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -18,17 +22,16 @@ public class Member {
     private Long id;
     private String name;
 
-    //Period
-//    private LocalDateTime startDate;
-//    private LocalDateTime endDate;
-    @Embedded
-    private Period period;
-
-    //Address
-//    private String city;
-//    private String street;
-//    private String zipcode;
-    @Embedded
-    private Address address;
+    @ElementCollection
+    @CollectionTable(name="favorite_food", joinColumns =
+        @JoinColumn(name="member_id")
+    )
+    @Column(name="food_name")
+    private Set<String> favoriteFoods = new HashSet<>();
+    @ElementCollection
+    @CollectionTable(name="address", joinColumns =
+        @JoinColumn(name="member_id")
+    )
+    private List<Address> addressHistory = new ArrayList<>();
     // 로직이 들어가면 getter, setter 사용 지양
 }
